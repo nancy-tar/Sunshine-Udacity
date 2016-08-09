b5746d33f8109c61586808d21a8c49e35b23bfee
nancy-tar <rinraven@gmail.com>
2016-08-09 12:12:09 +0300

    6 lesson start

--- app/src/main/java/com/example/anastasia/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/anastasia/sunshine/ForecastFragment.java
@@ -20,6 +20,7 @@ import android.widget.ListView;
 
 
 import com.example.anastasia.sunshine.data.WeatherContract;
+import com.example.anastasia.sunshine.service.SunshineService;
 
 
 /**
@@ -241,10 +242,10 @@ public class ForecastFragment extends Fragment  implements LoaderManager.LoaderC
      * Helper function to update weather data from the server
      */
     private void updateWeather(){
-
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
-        String location = Utility.getPreferredLocation(getActivity());
-        weatherTask.execute(location);
+        Intent intent = new Intent(getActivity(), SunshineService.class);
+        intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA,
+        Utility.getPreferredLocation(getActivity()));
+        getActivity().startService(intent);
     }
 
 
